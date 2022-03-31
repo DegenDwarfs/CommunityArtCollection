e89b595d41661af0eb47b939450324b056113112
stinkyfi <stinkyzeroex@gmail.com>
2022-03-31 16:16:52 -0700

    Current version is having display issues on OpenSea testnet. Mint price is impressive so far

--- contracts/DDCAC.sol
+++ contracts/DDCAC.sol
@@ -16,12 +16,9 @@ pragma solidity ^0.8.0;
 
 import "./ERC1155D.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title ERC1155Mock
- * This mock just publicizes internal functions for testing purposes
- */
-contract DDCAC is ERC1155 {
+contract DDCAC is ERC1155, Ownable {
     
     using Counters for Counters.Counter;
 
@@ -34,26 +31,48 @@ contract DDCAC is ERC1155 {
         _setURI(newuri);
     }
 
-    function mint(
-        address to
-    ) public {
+    function mint (address to) public onlyOwner {
         _tokenIds.increment();
         _mint(to, _tokenIds.current(), 1, '');
     }
 
-    function burn(
-        address owner,
-        uint256 id,
-        uint256 value
-    ) public {
+    function burn(address owner, uint256 id, uint256 value) public {
         _burn(owner, id, value);
     }
 
-    function burnBatch(
-        address owner,
-        uint256[] memory ids,
-        uint256[] memory values
-    ) public {
+    function burnBatch(address owner, uint256[] memory ids, uint256[] memory values) public {
         _burnBatch(owner, ids, values);
     }
+
+    // Private functions
+    /* @notice Returns the baseURI */         
+    function tokenURI(uint256 tokenId) public view virtual returns (string memory) {
+        return string(abi.encodePacked(uri(tokenId), toString(tokenId), ".json"));
+    }    
+
+    function toString(uint256 value) internal pure returns (string memory) {
+    // Inspired by OraclizeAPI's implementation - MIT license
+    // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+        if (value == 0) {
+            return "0";
+        }
+
+        uint256 temp = value;
+        uint256 digits;
+
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        bytes memory buffer = new bytes(digits);
+
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+
+        return string(buffer);
+    }    
 }
